d1f63f78d9ea2f701d6263ee1bc48941152632df
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-07 17:32:48 +0530

    Adding Deploy File

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+require('dotenv').config()
+
+const { interface, bytecode } = require("./compile");
+const { WALLET_MNM, WALLET_PROVIDER_URL } = process.env;
+const provider = new HDWalletProvider(WALLET_MNM, WALLET_PROVIDER_URL);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from account ", accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: "1000000" });
+
+    console.log("Contract deployed to ", result.options.address, " This is our Contract Address");
+    provider.engine.stop();
+}
+
+deploy();
